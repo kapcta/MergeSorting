b3792bf717801f7fe08bb315cc7eb8afad5c36c2
milovanov <milovanov.nikitos98@gmail.com>
2023-01-04 14:27:23 +0700

    Working version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Map<Integer, BufferedReader> READERS = CreatorOfReaders.createOfReaders(args);
         List<Integer> result = SortingFunc.sort(READERS);
+        System.out.println(result);
         WritingToFile.write(result, args);
     }
 
